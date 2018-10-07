62c96e2b27572135afc09457e884acbae92e8a42
FinneganMPike <33106706+FinneganMPike@users.noreply.github.com>
2018-10-07 15:04:51 -0400

    This is the addition of general formatting for the website.
    General Formatting

---
+++ resources/general/footer.js
@@ -0,0 +1,33 @@
+//<script type="text/javascript" src="../resources/general/footer.js"></script>
+
+//this is the javascript making the button that allows the user to 
+//scroll to the top
+
+// When the button is clicked, scroll to the top of the page
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+//when the page scrolls wun the scrollFunction
+window.onscroll = function() {scrollFunction()};
+
+//when the page is at the top no button is seen, but once the page 
+//is scrolled the button appears
+function scrollFunction() 
+{
+    if (document.body.scrollTop > 30 || document.documentElement.scrollTop > 30) 
+    {
+        document.getElementById("topBtn").style.display = "block";
+    } 
+    else 
+    {
+        disappearButton();
+    }
+}
+
+//when the page is at the top and on page load don't show 
+//the button
+function disappearButton()
+{
+  document.getElementById("topBtn").style.display = "none";
+}
