9a638305b26602c3848d0ec7485a4a62b594424c
arronlook <33300146+arronlook@users.noreply.github.com>
2018-10-07 21:10:42 -0400

    Added tutorial php and fixed generalcss

---
+++ user/tutorials/tutorial_landing_page.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en-US">
+<head>
+  <meta charset = "UTF-8">
+  <meta name="author" content="Arron">
+  <title>Tutorials</title>
+  <link rel="stylesheet" type="text/css" href="../../resources/general/general_content.css">
+</head>
+<?php
+require'../../resources/general/logo_user.html';
+require '../../resources/general/navbar_user.html';
+require '../../resources/general/footer.html';
+?>
