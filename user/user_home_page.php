90da723347fbbf167443b50b22b3d31f5f811727
arronlook <33300146+arronlook@users.noreply.github.com>
2018-10-21 15:41:39 -0400

    Updated user home and tutorial landing page

--- user/user_home_page.php
+++ user/user_home_page.php
@@ -9,10 +9,10 @@ require "../resources/general/start.php";
 	<meta name="author" content="Arron">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>HACK&/</title>
-	<link rel="stylesheet" type="text/css" href="../../resources/general/general_content.css">
-    <link rel="stylesheet" type="text/css" href="../../resources/tutorial_home/tutorial_home.css">
-    <script type="text/javascript" src="../../resources/general/footer.js"></script>
-    <script type="text/javascript" src="resources/general/cookies_enabled.js"></script>
+	<link rel="stylesheet" type="text/css" href="../resources/general/general_content.css">
+    <link rel="stylesheet" type="text/css" href="../resources/user_home/general_content.css">
+    <script type="text/javascript" src="../resources/general/footer.js"></script>
+    <script type="text/javascript" src="../resources/general/cookies_enabled.js"></script>
 </head>
 
 <?php 
@@ -22,17 +22,17 @@ require "../resources/general/start.php";
 
   <div id="body">
     <div id="container">
-      <div class="left" onclick="location.href='https://www.youtube.com/watch?v=dQw4w9WgXcQ'">
+      <div class="left option" onclick="location.href='https://www.youtube.com/watch?v=dQw4w9WgXcQ'">
         <h4>TUTORIALS</h4>
         <img alt="Tutorials Page Image" src="https://s3.amazonaws.com/coderbytestaticimages/homepage_icons/_right1_edit.jpg"/>
       </div>
-      <div class="left" onclick="location.href='https://www.youtube.com/watch?v=dQw4w9WgXcQ'">
+      <div class="left option" onclick="location.href='https://www.youtube.com/watch?v=dQw4w9WgXcQ'">
         <h4>CHALLENGES</h4>
         <img alt="Challenges Page Image" src="https://dmcommunity.files.wordpress.com/2014/09/challenge.jpg?w=112&h=150">
       </div>
-      <div class="left" onclick="location.href='https://www.youtube.com/watch?v=dQw4w9WgXcQ'">
+      <div class="left option" onclick="location.href='https://www.youtube.com/watch?v=dQw4w9WgXcQ'">
         <h4>SCOREBOARD</h4>
-        <img alt="Scoreboard Page Image" src="#">
+        <img alt="Scoreboard Page Image" src="http://i2.wp.com/www.maplestory2training.com/wp-content/uploads/maplestory-2-mano.png?resize=550%2C300">
       </div>
     </div>
   </div>
