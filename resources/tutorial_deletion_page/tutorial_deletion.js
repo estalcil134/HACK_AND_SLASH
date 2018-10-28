f1a10f991c8ca69af514483ec077d939e00dd562
Sebastian Castillo-Sanchez <scastillosanchez@outlook.com>
2018-10-28 15:45:33 -0400

    Add file deletion

--- resources/tutorial_deletion_page/tutorial_deletion.js
+++ resources/tutorial_deletion_page/tutorial_deletion.js
@@ -1,15 +1,29 @@
 //getting all the delete buttons on page to delete from page
-let deleteButtons = document.querySelectorAll('.delete-button');
+function deleteTutorial() {
+    let deleteButtons = document.querySelectorAll('.delete-button');
 
-for (let i = 0; i < deleteButtons.length; i++) {
-    deleteButtons[i].addEventListener('click', function(event) {
-        event.preventDefault();
+    for (let i = 0; i < deleteButtons.length; i++) {
+        deleteButtons[i].addEventListener('click', function(event) {
+            event.preventDefault();
 
-        let choice = confirm("Are you sure to delete this tutorial?");
+            let choice = confirm("Are you sure to delete this tutorial?");
 
-        if (choice) { //will delete tutorial div if confirm is "yes"
-            //Probably need a cookie function/tool to permanently delete a tutorial from page
-            deleteButtons[i].closest('.tutorial').remove();
-        }
-    });
+            if (choice) { //will delete tutorial from server and remove div if confirm is "yes"
+                /* Wait for tutorials to be in server
+                $.ajax({
+                    url: "delete.php",
+                    data: {'file': "<?php echo dirname(__FILE__) . '/tutorials/'?>" + "file_name"}, //add input variable or array for filename?
+                    success: function () {
+                        alert("Tutorial deleted!");
+                        deleteButtons[i].closest('.tutorial').remove();
+                    },
+                    error: function () {
+                        alert("Tutorial could not be deleted!");
+                    }
+                });
+                */
+                deleteButtons[i].closest('.tutorial').remove();
+            }
+        });
+    }
 }
