5d90b42391617d89bad9474cbc9910d8bcb302ed
Sebastian Castillo-Sanchez <scastillosanchez@outlook.com>
2018-11-18 14:09:12 -0500

    Add files

--- resources/tutorial_deletion_page/tutorial_deletion.js
+++ resources/tutorial_deletion_page/tutorial_deletion.js
@@ -9,20 +9,20 @@ function deleteTutorial() {
             let choice = confirm("Are you sure to delete this tutorial?");
 
             if (choice) { //will delete tutorial from server and remove div if confirm is "yes"
-                /* Wait for tutorials to be in server
-                $.ajax({
-                    url: "delete.php",
-                    data: {'file': "<?php echo dirname(__FILE__) . '/tutorials/'?>" + "file_name"}, //add input variable or array for filename?
-                    success: function () {
-                        alert("Tutorial deleted!");
-                        deleteButtons[i].closest('.tutorial').remove();
-                    },
-                    error: function () {
-                        alert("Tutorial could not be deleted!");
-                    }
-                });
-                */
-                deleteButtons[i].closest('.tutorial').remove();
+                //Wait for tutorials to be in server
+                // $.ajax({
+                //     url: "delete.php",
+                //     data: {'file': "<?php echo dirname(__FILE__) . '/tutorials/'?>" + "file_name"}, //add input variable or array for filename?
+                //     success: function () {
+                //         alert("Tutorial deleted!");
+                //         deleteButtons[i].closest('.tutorial').remove();
+                //     },
+                //     error: function () {
+                //         alert("Tutorial could not be deleted!");
+                //     }
+                // });
+                //
+                // deleteButtons[i].closest('.tutorial').remove();
             }
         });
     }
