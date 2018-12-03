4866995b09fb42107a97c9674115b75c6604077a
Wilson Wong <wilsonwong1688@gmail.com>
2018-12-03 00:20:36 -0500

    empty_div for Finn
    as requested

--- admin/admin_home_page.php
+++ admin/admin_home_page.php
@@ -28,13 +28,24 @@ require '../resources/general/navbar_user.html';
 <?php require '../resources/general/footer.html'; ?>
 
 <body>
+	<nav>
+		<ul id="naver">
+			<li class="nav right" id = "nav_right"><a class="right nav" href="/index.php">Logout</a></li>
+		</ul>
+	</nav>
 	<h1>Admin Home Page</h1>
-	<div class="container">
-		<div class="left">
-			
+	<div class="w_container">
+		<div class="w_left">
+			<a href="tutorial_creation_page/#"><p>Tutorial Creation</p></a>
+			<a href="tutorial_deletion_page/tutorial_deletion_page.php"><p>Tutorial Deletion</p></a>
+			<a href="../user/tutorials/tutorial_landing_page.php"><p>Tutorials</p></a>
+			<a href="meta_tutorial/meta_tutorial.html"><p>MetaTutorial</p></a>
 		</div>
-		<div class="right">
-			
+		<div class="w_right">
+			<a href="challenge_creation_page/challenge_creation.php"><p>Challenge Creation</p></a>
+			<a href="challenge_deletion_page/challenge_deletion_page.php"><p>Challenge Deletion</p></a>
+			<a href="../user/challenges/challenge_landing_page.php"><p>Challenges</p></a>
+			<a href="../user/scoreboard/scoreboard.php"><p>Scoreboard</p></a>
 		</div>
 	</div>
 </body>
